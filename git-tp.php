48bec3c26338d228ee56b5a1cf57b4d2aac52764
Daniel Kurzmann <daniel.kurzmann@aoe.com>
2017-01-31 10:46:16 +0100

    Split functionality from start script

--- git-tp.php
+++ git-tp.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/helper/targetProcessHelper.php';
 require_once __DIR__ . '/helper/emailHelper.php';
 require_once __DIR__ . '/helper/hashHelper.php';
+require_once __DIR__ . '/helper/gitTpHelper.php';
 
 
 if (isset($argv[1]) && $argv[1] == 'help') {
@@ -24,42 +25,9 @@ USAGE;
     die;
 }
 
-if (isset($argv[1])) {
-    $hashHelper = new HashHelper();
-    $tp = new TargetProcessHelper($configuration);
-    switch ($argv[1]) {
-        case 'getStartHash':
-            $hashHelper->getStartHash($argv[2]);
-            break;
-        case 'getEndHash':
-            $hashHelper->getEndHash($argv[2]);
-            break;
-        case 'createGitLog':
-            shell_exec('git -C ' . $argv[1] . ' log --pretty=oneline ' . $argv[2] . '...' . $argv[3] . ' > git.log');
-            break;
-        case 'addTpTag':
-            $tag = isset($argv[2]) ? $argv[2] : null;
-            $addTicket = true;
+new GitTpHelper($configuration);
 
-            $tickets = $tp->addTagsToTargetProcessTickets($addTicket, $tag);
-            print_r($tickets);
-            break;
-        case 'sendChangelog':
-            $tag = isset($argv[2]) ? $argv[2] : null;
-            $addTicket = false;
-            $sendMail = isset($argv[3]) ? $argv[3] : false;
 
-            $tp = new TargetProcessHelper($configuration);
-            $tickets = $tp->addTagsToTargetProcessTickets($addTicket, $tag);
-            print_r($tickets);
-
-            if (is_string($sendMail)) {
-                $emailHelper = new EmailHelper($configuration);
-                $emailHelper->sendMail($sendMail, $tickets, $tag);
-            }
-            break;
-    }
-}
 
 
 
---
+++ helper/gitTpHelper.php
@@ -0,0 +1,52 @@
+<?php
+class GitTpHelper
+{
+    protected $_configuration = [];
+
+    public function __construct($configuration = [])
+    {
+        if ($configuration) {
+            $this->_configuration = $configuration;
+        }
+    }
+
+    public function run()
+    {
+        if (isset($argv[1])) {
+            $hashHelper = new HashHelper();
+            $tp = new TargetProcessHelper($this->_configuration);
+            $emailHelper = new EmailHelper($this->_configuration);
+
+            switch ($argv[1]) {
+                case 'getStartHash':
+                    $hashHelper->getStartHash($argv[2]);
+                    break;
+                case 'getEndHash':
+                    $hashHelper->getEndHash($argv[2]);
+                    break;
+                case 'createGitLog':
+                    shell_exec('git -C ' . $argv[1] . ' log --pretty=oneline ' . $argv[2] . '...' . $argv[3] . ' > git.log');
+                    break;
+                case 'addTpTag':
+                    $tag = isset($argv[2]) ? $argv[2] : null;
+                    $addTicket = true;
+
+                    $tickets = $tp->addTagsToTargetProcessTickets($addTicket, $tag);
+                    print_r($tickets);
+                    break;
+                case 'sendChangelog':
+                    $tag = isset($argv[2]) ? $argv[2] : null;
+                    $addTicket = false;
+                    $sendMail = isset($argv[3]) ? $argv[3] : false;
+
+                    $tickets = $tp->addTagsToTargetProcessTickets($addTicket, $tag);
+                    print_r($tickets);
+
+                    if (is_string($sendMail)) {
+                        $emailHelper->sendMail($sendMail, $tickets, $tag);
+                    }
+                    break;
+            }
+        }
+    }
+}
