0de596732c0cb7f7acea000169ac1e303fee88ac
Daniel Kurzmann <daniel.kurzmann@aoe.com>
2017-01-31 11:33:57 +0100

    Bugfix: function paramater was missing

--- git-tp.php
+++ git-tp.php
@@ -6,6 +6,12 @@ if (!class_exists('SesClient')) {
 
 require_once __DIR__ . '/config.php';
 
+require_once __DIR__ . '/helper/targetProcessHelper.php';
+require_once __DIR__ . '/helper/emailHelper.php';
+require_once __DIR__ . '/helper/hashHelper.php';
+require_once __DIR__ . '/helper/gitTpHelper.php';
+
+
 if (isset($argv[1]) && $argv[1] == 'help') {
     echo <<<USAGE
 Usage:  php git-tp.php getStartHash <version.txt url>
