03cd294637086cff2faa40a1ada30e0fa3a2c5ea
Daniel Kurzmann <daniel.kurzmann@aoe.com>
2017-04-26 16:48:27 +0200

    refactor stuff

--- index.php
+++ index.php
@@ -25,41 +25,51 @@ $team = isset($_POST['team']) ? $_POST['team'] : isset($configuration['defaultTe
 $project = isset($_POST['project']) ? $_POST['project'] : isset($configuration['defaultProjectName']) ? $configuration['defaultProjectName'] : "";
 $from = isset($_POST['from']) ? $_POST['from'] : $thisMonth;
 $to = isset($_POST['to']) ? $_POST['to'] : $nextMonth;
-$_release = isset($_POST['group']) && $_POST['group'] == 'Release' ? true : false;
+$release = !isset($_POST['group']) || $_POST['group'] == 'Release' ? true : false;
+$forFilter = $release ? 'Release' : 'TeamIteration';
 
 ?>
         Team: <input type="text" name="team" value="<?php echo $team ?>"><br>
         Project: <input type="text" name="project" value="<?php echo $project ?>"><br>
         From: <input type="text" name="from" value="<?php echo $from ?>"><br>
         To: <input type="text" name="to" value="<?php echo $to ?>"><br>
-        Releases <input type="radio" name="group" value="Release" <?= $_release ? "checked" : "" ?>><br>
-        Team Iterations <input type="radio" name="group" value="TeamIteration" <?= !$_release ? "checked" : "" ?>><br>
+        Releases <input type="radio" name="group" value="Release" <?= $release ? "checked" : "" ?>><br>
+        Team Iterations <input type="radio" name="group" value="TeamIteration" <?= !$release ? "checked" : "" ?>><br>
     <input type="submit">
 </form>
     <?php
 
-$filter = "?where=(Team.Name eq '" . $team . "') and (Project.Name eq '" . $project . "') and (" . $_POST['group'] . ".StartDate gte '" . $from . "') and (" . $_POST['group'] . ".EndDate lte '" . $to . "')";
+$filter = "";
+$assignables = [];
 
-$filter = str_replace('#', '%23', $filter);
-$filter = str_replace(' ', '%20', $filter);
+if (isset($_POST['group'])) {
+    $filter = "?where=(Team.Name eq '" . $team . "') and (Project.Name eq '" . $project . "') and (" . $forFilter . ".StartDate gte '" . $from . "') and (" . $forFilter . ".EndDate lte '" . $to . "')";
 
-//gets Assignables
-$targetProcessHelper = new TargetProcessHelper($configuration);
-$assignables = $targetProcessHelper->getAssignables($filter);
+    $filter = str_replace('#', '%23', $filter);
+    $filter = str_replace(' ', '%20', $filter);
+    //gets Assignables
+    $targetProcessHelper = new TargetProcessHelper($configuration);
+    $assignables = $targetProcessHelper->getAssignables($filter);
 
-$informationArray['Name'] = $from . ' to ' . $to;
+    $informationArray['Name'] = $from . ' to ' . $to;
 
-foreach ($assignables as $key => $entity) {
-    if ($entity['EntityType']['Name'] == 'UserStory')
-        $informationArray['UserStories'][] = $entity;
-    else
-        $informationArray['Bugs'][] = $entity;
+    foreach ($assignables as $key => $entity) {
+        if ($entity['EntityType']['Name'] == 'UserStory')
+            $informationArray['UserStories'][] = $entity;
+        else
+            $informationArray['Bugs'][] = $entity;
+    }
+
+    //formats the information into a list
+    $reviewOutput = new ReviewHelper($configuration);
+    $information = $reviewOutput->generateOutputForEntities($informationArray);
+
+    //direct output
+    $pagehelper = new ReviewPageHelper();
+    $pagehelper->printArray($information);
+
 }
 
-//formats the information into a list
-$reviewOutput = new ReviewHelper($configuration);
-$information = $reviewOutput->generateOutputForEntities($informationArray);
 
-//direct output
-$pagehelper = new ReviewPageHelper();
-$pagehelper->printArray($information);
+
+
